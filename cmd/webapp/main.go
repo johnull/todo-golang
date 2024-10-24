b62a6ccc9b0258dece3f3210e082f6f896e002fa
jhng0d <johnathantostudy@gmail.com>
2024-10-24 13:05:16 -0300

    Add: Router hander & Graceful server shutdown

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/johnull/todo-golang/internal/controllers"
 	"github.com/joho/godotenv"
-	"github.com/johnull/todo-golang/controllers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -14,13 +21,41 @@ func main() {
 	}
 
 	/* ROUTES */
-	router := http.NewServeMux()
-	router.HandleFunc("GET /", controllers.GetItems)
-	router.HandleFunc("POST /add", controllers.AddItem)
-	router.HandleFunc("POST /complete/{id}", controllers.CompleteItem)
-	router.HandleFunc("POST /delete/all", controllers.DeleteCompleted)
+	router := mux.NewRouter()
+	router.HandleFunc("/", controllers.GetItems)
+	router.HandleFunc("/add", controllers.AddItem).Methods("POST")
+	router.HandleFunc("/complete/{id}", controllers.CompleteItem).Methods("POST")
+	router.HandleFunc("/delete/all", controllers.DeleteCompleted).Methods("POST")
 
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatal(err)
-	} 
+	// http server config
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
+	
+	// starts the server in a goroutine to avoid blocking execution
+	go func() {
+
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("Server stopped")
+	}()
+
+	// channel to catch SIGINT & SIGTERM from the OS
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	// waits interrupt signals 
+	<-sig
+
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// wait for the graceful shutdown of the server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP close error: %v", err)
+	}
+	log.Println("Server shutdown.")
 }
