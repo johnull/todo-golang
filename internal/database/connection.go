132188f9bf6d1ceca18307ead02e4581fb487ec9
jhng0d <johnathantostudy@gmail.com>
2024-10-24 16:18:53 -0300

    add db migrate

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -28,15 +28,11 @@ func ConnectDB() *sql.DB {
 		log.Fatal("DB_PASS")
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/", username, password, host)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/todoDB", username, password, host)
 
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Database connection error: %v", err)
-	}
-
-	if err := setupDatabase(db); err != nil {
-		log.Fatalf("setup database error: %v", err)
 	}
 
 	log.Println("Connected to database")
